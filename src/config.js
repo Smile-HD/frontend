6ad377cb7ac877adb317108d8935e63c13bd08f2
Smile-HD <erickmiguel2022@hotmail.com>
2025-11-23 18:09:13 -0400

    afk

--- src/config.js
+++ src/config.js
@@ -4,7 +4,7 @@
 // Para cambiar el servidor backend, modifica la URL aquí abajo:
 
 // Cuando subas tu backend a Render, reemplaza con tu URL de Render:
-export const API_URL = 'https://backend-si1-hqtg.onrender.com'
+export const API_URL = 'https://backend-si1-hqtg.onrender.com/api'
 
 // Ejemplos de otras URLs que puedes usar:
 // export const API_URL = 'http://192.168.1.100:3000/api'  // Servidor en red local
